712ab30947629214c9dda0e7de4fcceb6d63ac52
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-09-06 19:32:25 +0200

    fix external dependencies

--- extern/KaGen.cmake
+++ extern/KaGen.cmake
@@ -1,7 +1,16 @@
 cmake_minimum_required(VERSION 3.18)
 
 block()
-    include(FetchContent)
+
+    set(KAGEN_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(KAGEN_BUILD_APPS OFF CACHE BOOL "" FORCE)
+    set(KAGEN_BUILD_TOOLS OFF CACHE BOOL "" FORCE)
+    set(KAGEN_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+    set(KAGEN_USE_XXHASH OFF CACHE BOOL "" FORCE)
+    set(KAGEN_WARNINGS_ARE_ERRORS OFF CACHE BOOL "" FORCE)
+    set(INSTALL_KAGEN OFF CACHE BOOL "" FORCE)
+
     FetchContent_Declare(KaGen GIT_REPOSITORY https://github.com/sebalamm/KaGen.git GIT_TAG master)
     FetchContent_MakeAvailable(KaGen)
+
 endblock()
