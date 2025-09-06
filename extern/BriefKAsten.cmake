f6f8ad1ecb8f6a361f04461c03acd0c5aab9d7dd
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-09-06 19:32:25 +0200

    fix external dependencies

--- extern/BriefKAsten.cmake
+++ extern/BriefKAsten.cmake
@@ -1,7 +1,12 @@
 cmake_minimum_required(VERSION 3.18)
 
 block()
-    include(FetchContent)
+
+    set(BRIEFKASTEN_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+    set(BRIEFKASTEN_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(BRIEFKASTEN_USE_CXX23 ON CACHE BOOL "" FORCE)
+
     FetchContent_Declare(BriefKAsten GIT_REPOSITORY https://github.com/niklas-uhl/BriefKAsten GIT_TAG main)
     FetchContent_MakeAvailable(BriefKAsten)
+
 endblock()
--- extern/stxxl.cmake
+++ extern/stxxl.cmake
@@ -1,77 +1,47 @@
-# set(stxxl_SOURCE_DIR "${CMAKE_BINARY_DIR}/_deps/stxxl-src")
-# set(stxxl_BINARY_DIR "${CMAKE_BINARY_DIR}/_deps/stxxl-src/build")
-# set(stxxl_INSTALL_DIR "${CMAKE_BINARY_DIR}/_deps/stxxl")
-# set(stxxl_STAMP "${CMAKE_CURRENT_BINARY_DIR}/stxxl.stamp")
-#
-# set(stxxl_BUILD_NOW OFF)
-# if (NOT EXISTS "${stxxl_STAMP}")
-#     set(stxxl_BUILD_NOW ON)
-#     # file(REMOVE_RECURSE "${stxxl_SOURCE_DIR}")
-#     file(MAKE_DIRECTORY
-#             "${CMAKE_BINARY_DIR}"
-#             "${CMAKE_BINARY_DIR}/_deps"
-#             "${CMAKE_BINARY_DIR}/_deps/stxxl-src"
-#             RESULT MAKE_DIRECTORY_RESULT)
-#     if (NOT MAKE_DIRECTORY_RESULT EQUAL 0)
-#         message(FATAL_ERROR "Failed to create directory: ${stxxl_SOURCE_DIR}")
-#     endif ()
-#     execute_process(
-#             COMMAND git clone
-#             --recurse-submodules
-#             --branch 1.4.1
-#             --single-branch
-#             --depth 1
-#             https://github.com/stxxl/stxxl.git
-#             "${stxxl_SOURCE_DIR}"
-#             OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
-# else ()
-#     file(GLOB_RECURSE stxxl_SOURCES "${stxxl_SOURCE_DIR}/src/*")
-#     foreach (stxxl_SOURCE IN LISTS stxxl_SOURCES)
-#         if (EXISTS "${stxxl_SOURCE}" AND "${stxxl_SOURCE}" IS_NEWER_THAN "${stxxl_STAMP}")
-#             set(stxxl_BUILD_NOW ON)
-#             break()
-#         endif ()
-#     endforeach ()
-# endif ()
-#
-# if (${stxxl_BUILD_NOW})
-#     execute_process(
-#             COMMAND cmake -S "${stxxl_SOURCE_DIR}" -B "${stxxl_BINARY_DIR}"
-#             "-DBUILD_TESTS=OFF"
-#             "-DBUILD_TESTING=OFF"
-#             "-DBUILD_EXTRAS=OFF"
-#             "-DBUILD_EXAMPLES=OFF"
-#             "-DBUILD_SHARED_LIBS=OFF"
-#             "-DUSE_VALGRIND=OFF"
-#             "-DUSE_GCOV=OFF"
-#             "-DUSE_TPIE=OFF"
-#             "-DCMAKE_MESSAGE_LOG_LEVEL=NOTICE"
-#             "-DCMAKE_SUPPRESS_DEVELOPER_WARNINGS=1"
-#             "-DCMAKE_WARN_DEPRECATED=OFF"
-#             "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
-#             "-DCMAKE_INSTALL_PREFIX=${stxxl_INSTALL_DIR}"
-#             WORKING_DIRECTORY "${stxxl_SOURCE_DIR}"
-#             OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
-#     execute_process(
-#             COMMAND cmake --build "${stxxl_BINARY_DIR}"
-#             WORKING_DIRECTORY "${stxxl_SOURCE_DIR}"
-#             OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
-#     execute_process(
-#             COMMAND cmake --install "${stxxl_BINARY_DIR}"
-#             WORKING_DIRECTORY "${stxxl_SOURCE_DIR}"
-#             OUTPUT_QUIET COMMAND_ERROR_IS_FATAL ANY)
-#     file(TOUCH "${stxxl_STAMP}")
-# endif ()
-# list(APPEND CMAKE_PREFIX_PATH "${stxxl_INSTALL_DIR}")
-#
-# find_package(stxxl REQUIRED)
-# add_library(stxxl::stxxl ALIAS stxxl)
-#
-
 cmake_minimum_required(VERSION 3.18)
 
 block()
-    include(FetchContent)
+
+    cmake_policy(SET CMP0048 NEW)
+    cmake_policy(SET CMP0069 OLD)
+    cmake_policy(SET CMP0077 NEW)
+
+    set(CMAKE_POLICY_DEFAULT_CMP0048 NEW)
+    set(CMAKE_POLICY_DEFAULT_CMP0069 OLD)
+    set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
+
+    set(INTERPROCEDURAL_OPTIMIZATION OFF)
+
+    set(BUILD_EXAMPLES OFF)
+    set(BUILD_TESTS OFF)
+    set(BUILD_EXTRAS OFF)
+    set(USE_BOOST OFF)
+    set(USE_VALGRIND OFF)
+    set(USE_GCOV OFF)
+    set(USE_TPIE OFF)
+    set(BUILD_STATIC_LIBS ON)
+    set(BUILD_SHARED_LIBS OFF)
+
     FetchContent_Declare(stxxl GIT_REPOSITORY https://github.com/stxxl/stxxl.git GIT_TAG 1.4.1)
-    FetchContent_MakeAvailable(stxxl)
+    FetchContent_GetProperties(stxxl)
+    if (NOT stxxl_POPULATED)
+        FetchContent_Populate(stxxl)
+        execute_process(COMMAND
+                ${GIT_EXECUTABLE} -C "${stxxl_SOURCE_DIR}" apply --check "${TARGET_DIR}/stxxl.patch"
+                RESULT_VARIABLE PATCH_ALREADY_APPLIED
+                OUTPUT_QUIET ERROR_QUIET)
+        if (PATCH_ALREADY_APPLIED EQUAL 0)
+            execute_process(COMMAND ${GIT_EXECUTABLE} -C "${stxxl_SOURCE_DIR}" apply "${TARGET_DIR}/stxxl.patch")
+        endif ()
+        add_subdirectory(${stxxl_SOURCE_DIR} ${stxxl_BINARY_DIR})
+    endif ()
+
+    # suppress compilation warnings for external target
+    target_compile_options(stxxl PRIVATE
+            $<$<CXX_COMPILER_ID:GNU>:-w>
+            $<$<CXX_COMPILER_ID:Clang>:-w>
+            $<$<CXX_COMPILER_ID:MSVC>:/W0>)
+
+    add_library(stxxl::stxxl ALIAS stxxl)
+
 endblock()
