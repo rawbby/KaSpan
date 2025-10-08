057838fc6753f94204e8c369c08cc24df9a7ad54
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-10-08 15:40:54 +0200

    fix

--- extern/extern.cmake
+++ extern/extern.cmake
@@ -33,6 +33,6 @@ block()
     include("${TARGET_DIR}/kamping.cmake")
     include("${TARGET_DIR}/stxxl.cmake")
     include("${TARGET_DIR}/KaGen.cmake")
-    include("${TARGET_DIR}/BriefKAsten.cmake")
+    # include("${TARGET_DIR}/BriefKAsten.cmake")
 
 endblock()
