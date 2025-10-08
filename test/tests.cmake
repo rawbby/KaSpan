3129149c23a74f96f0338b812117d40fb07de6d3
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-10-08 15:39:17 +0200

    fix

--- test/tests.cmake
+++ test/tests.cmake
@@ -1,7 +1,6 @@
 target_link_libraries(${TARGET_NAME} PRIVATE
         essential partition graph scc
         kamping::kamping
-        BriefKAsten::BriefKAsten
         KaGen::KaGen
         stxxl::stxxl)
 target_compile_definitions(${TARGET_NAME} PRIVATE KASPAN_NORMALIZE)
