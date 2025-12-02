c58b47b9f042c8ceadbc24300ab5b19bf59bcf09
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-12-02 13:34:52 +0100

    step

--- experiment/horeka.cmd.sh
+++
@@ -1,8 +0,0 @@
-I_MPI_PIN=1                       \
-I_MPI_PIN_DOMAIN=core             \
-I_MPI_PIN_ORDER=compact           \
-I_MPI_JOB_TIMEOUT=${TIME_SECONDS} \
-mpiexec.hydra                     \
-  -n ${NTASKS}                    \
-  -bootstrap slurm                \
-  ${CMD}
