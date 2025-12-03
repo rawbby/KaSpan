8f4c86a737cba7a4869738a19062da519cd4a49c
Robert Andreas Fritsch <git@robert-fritsch.de>
2025-12-03 08:51:17 +0100

    extend plotting

--- experiment/horeka/strong/rwd/plot.py
+++ experiment/horeka/strong/rwd/plot.py
@@ -56,11 +56,11 @@ app_style = {app: next(style_cycle) for app in apps}
 for app_graph, values in duration_data.items():
     app, graph = app_graph
     nps = sorted(values.keys())
-    durations = [float(values[n]) * 10e-9 for n in nps]
+    memory = [float(values[n]) * 10e-9 for n in nps]
 
     plt.figure()
     color, marker = app_style[app]
-    plt.plot(nps, durations, color=color, marker=marker)
+    plt.plot(nps, memory, color=color, marker=marker)
     plt.xlabel("np")
     plt.ylabel("seconds")
     plt.title(f"Strong Scaling {app} '{graph}'")
@@ -91,15 +91,48 @@ for graph in graphs:
     plt.figure()
     for app, values in data.items():
         # build durations aligned with union nps, use NaN for missing
-        durations = [values.get(n, float("nan")) for n in nps]
+        memory = [values.get(n, float("nan")) for n in nps]
         color, marker = app_style[app]
-        plt.plot(nps, durations, label=app, color=color, marker=marker)
+        plt.plot(nps, memory, label=app, color=color, marker=marker)
 
     plt.xlabel("np")
     plt.ylabel("seconds")
     plt.title(f"Strong Scaling '{graph}'")
     plt.grid(True)
-    plt.yscale("log", base=10)  # use base-10 log scale on seconds
+    plt.yscale("log", base=10)
+    plt.xscale("log", base=2)
+    plt.legend()
+    plt.tight_layout()
+
+    #     plt.savefig(cwd / f"{graph}.png", dpi=200)
+    #     plt.close()
+    #
+    #     print(cwd / f"{graph}.png")
+    #
+    # for graph in graphs:
+
+    # map app -> {n: memory}
+    data = {
+        app_graph[0]: values
+        for app_graph, values in memory_data.items()
+        if app_graph[1] == graph
+    }
+
+    # union of all nps across apps
+    nps = sorted({n for values in data.values() for n in values.keys()})
+
+    plt.figure()
+    for app, values in data.items():
+        # build durations aligned with union nps, use NaN for missing
+        memory = [values.get(n, float("nan")) for n in nps]
+        color, marker = app_style[app]
+        plt.plot(nps, memory, label=app, color=color, marker=marker)
+
+    plt.xlabel("np")
+    plt.ylabel("bytes")
+    plt.title(f"Strong Scaling '{graph}'")
+    plt.grid(True)
+    plt.yscale("log", base=8)
     plt.xscale("log", base=2)
     plt.legend()
     plt.tight_layout()
