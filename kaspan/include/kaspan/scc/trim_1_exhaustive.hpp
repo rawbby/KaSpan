01428cc4c376645444dad6c2cdba8aaa522fe3dd
Robert Andreas Fritsch <git@robert-fritsch.de>
2026-01-13 15:28:01 +0100

    documenmt exhaustive trimming and normalize line endings to lf (hopefully)

--- kaspan/include/kaspan/scc/trim_1_exhaustive.hpp
+++ kaspan/include/kaspan/scc/trim_1_exhaustive.hpp
@@ -6,6 +6,46 @@
 
 namespace kaspan {
 
+/// trim_1_exhaustive iteratevely trims one direction, either forward or backward, exhaustive.
+/// To do that scc_id must be valid and degree must be valid if:
+/// scc_id[k] != undecided || valid(degree[k]).
+template<world_part_concept part_t>
+auto
+trim_1_exhaustive(part_t const& part, index_t const* head, index_t const* csr, vertex_t* scc_id, vertex_t* degree, vertex_frontier& frontier) -> vertex_t
+{
+  vertex_t decided_count = 0;
+
+  do {
+    while (frontier.has_next()) {
+
+      // Each vertex that we get from the queue
+      // has a neighbour that was decided. This
+      // is the location where we update the degree
+      // accordingly if this vertex is not alredy
+      // decided itself!
+
+      auto const u = frontier.next();
+      auto const k = part.to_local(u);
+
+      if (scc_id[k] == scc_id_undecided) {
+        if (--degree[k]; degree[k] == 0) { // decide and prepare to notify neighbours
+          scc_id[k] = u;
+          ++decided_count;
+          for (auto const v : csr_range(head, csr, k)) {
+            if (part.has_local(v)) frontier.local_push(v);
+            else frontier.push(part.world_rank_of(v), v);
+          }
+        }
+      }
+    }
+  } while (frontier.comm(part));
+
+  return decided_count;
+}
+
+/// trim_1_exhaustive_first iteratively trims vertices with indegree/outdegree of zero.
+/// It assumes to run on a fresh graph with uninitialised scc_id/indegree/outdegree and
+/// will initilise these appropriately.
 template<world_part_concept part_t>
 auto
 trim_1_exhaustive_first(part_t const&    part,
@@ -16,121 +56,64 @@ trim_1_exhaustive_first(part_t const&    part,
                         vertex_t*        scc_id,
                         vertex_t*        outdegree,
                         vertex_t*        indegree,
-                        vertex_frontier& frontier)
+                        vertex_frontier& frontier) -> vertex_t
 {
   auto const local_n       = part.local_n();
   vertex_t   decided_count = 0;
 
-  // initial trim forward
+  // == initial indegree trim ==
+  // We get the initial indegree right from the head array
+  // and decide vertices with indegree of zero right away.
+  // For every non resolved vertex we need to store the right
+  // indegree and mark as undecided. (As this function is
+  // expected to initialise the scc_id array)
   for (vertex_t k = 0; k < local_n; ++k) {
-    if (indegree[k] = bw_head[k + 1] - bw_head[k]; indegree[k] == 0) {
+    auto const degree = bw_head[k + 1] - bw_head[k];
+
+    if (degree > 0) { // initialise
+      scc_id[k]   = scc_id_undecided;
+      indegree[k] = degree;
+    }
+
+    else { // decide right away and prepare to notify neighbours
       scc_id[k] = part.to_global(k);
       ++decided_count;
       for (auto const v : csr_range(fw_head, fw_csr, k)) {
-        if (part.has_local(v)) {
-          frontier.local_push(v);
-        } else {
-          frontier.push(part.world_rank_of(v), v);
-        }
+        if (part.has_local(v)) frontier.local_push(v);
+        else frontier.push(part.world_rank_of(v), v);
       }
-    } else {
-      scc_id[k] = scc_id_undecided;
     }
   }
 
-  do {
-    while (frontier.has_next()) {
-      auto const k = part.to_local(frontier.next());
-      if (scc_id[k] == scc_id_undecided) {
-        if (--indegree[k]; indegree[k] == 0) {
-          scc_id[k] = part.to_global(k);
-          ++decided_count;
-          for (auto const v : csr_range(fw_head, fw_csr, k)) {
-            if (part.has_local(v)) {
-              frontier.local_push(v);
-            } else {
-              frontier.push(part.world_rank_of(v), v);
-            }
-          }
-        }
-      }
-    }
-  } while (frontier.comm(part));
+  // With the initialisation done and the first collected decision we
+  // can now simply delegate the exhaustive trimming to a sub routine.
+  decided_count += trim_1_exhaustive(part, fw_head, fw_csr, scc_id, indegree, frontier);
 
-  // initial trim backward
+  // == initial outdegree trim ==
+  // We do similar to the initial indegree trim
+  // but scc_id is already initialised and we might
+  // already have decided vertices to skip.
   for (vertex_t k = 0; k < local_n; ++k) {
     if (scc_id[k] == scc_id_undecided) {
-      if (outdegree[k] = fw_head[k + 1] - fw_head[k]; outdegree[k] == 0) {
+      auto const degree = fw_head[k + 1] - fw_head[k];
+
+      if (degree > 0) { // initialise
+        outdegree[k] = degree;
+      }
+
+      else { // decide right away and prepare to notify neighbours
         scc_id[k] = part.to_global(k);
         ++decided_count;
         for (auto const v : csr_range(bw_head, bw_csr, k)) {
-          if (part.has_local(v)) {
-            frontier.local_push(v);
-          } else {
-            frontier.push(part.world_rank_of(v), v);
-          }
+          if (part.has_local(v)) frontier.local_push(v);
+          else frontier.push(part.world_rank_of(v), v);
         }
       }
     }
   }
 
-  do {
-    while (frontier.has_next()) {
-      auto const k = part.to_local(frontier.next());
-      if (scc_id[k] == scc_id_undecided) {
-        if (--outdegree[k]; outdegree[k] == 0) {
-          scc_id[k] = part.to_global(k);
-          ++decided_count;
-          for (auto const v : csr_range(bw_head, bw_csr, k)) {
-            if (part.has_local(v)) {
-              frontier.local_push(v);
-            } else {
-              frontier.push(part.world_rank_of(v), v);
-            }
-          }
-        }
-      }
-    }
-  } while (frontier.comm(part));
-
-  degree max_degree{ .degree_product = std::numeric_limits<index_t>::min(), .u = scc_id_undecided };
-  for (vertex_t k = 0; k < local_n; ++k) {
-    if (scc_id[k] == scc_id_undecided) {
-      auto const prod = indegree[k] + outdegree[k];
-      DEBUG_ASSERT_GT(prod, 0);
-
-      if (prod > max_degree.degree_product) {
-        max_degree.degree_product = prod;
-        max_degree.u              = part.to_global(k);
-      }
-    }
-  }
-
-  return PACK(decided_count, max_degree);
-}
-
-template<world_part_concept part_t>
-auto
-trim_1_exhaustive(part_t const&   part,
-                  index_t const*  fw_head,
-                  vertex_t const* fw_csr,
-                  index_t const*  bw_head,
-                  vertex_t const* bw_csr,
-                  vertex_t*       scc_id,
-                  vertex_t*       fw_degree,
-                  vertex_t*       bw_degree) -> vertex_t
-{
-  auto const local_n       = part.local_n();
-  vertex_t   decided_count = 0;
-  for (vertex_t k = 0; k < local_n; ++k) {
-    if (scc_id[k] == scc_id_undecided) {
-      if (not fw_degree[k] or not bw_degree[k]) {
-        scc_id[k] = part.to_global(k);
-        ++decided_count;
-      }
-    }
-  }
-  return decided_count;
+  // delegate the exhaustive trimming to a sub routine again.
+  return decided_count + trim_1_exhaustive(part, fw_head, fw_csr, scc_id, indegree, frontier);
 }
 
 } // namespace kaspan
